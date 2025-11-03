d25371208bdba48dc5f21d9b5067968ceda3d695
Arnav66692 <105764398+Arnav66692@users.noreply.github.com>
2025-11-02 21:39:46 -0800

    Update CardTester1.java

--- src/main/java/Activity1/CardTester1.java
+++ src/main/java/Activity1/CardTester1.java
@@ -10,14 +10,17 @@ public class CardTester1 {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-		Card one = new Card("3", "Hearts", 3);
-  		Card two = new Card("2", "Spades",3);
-  		Card three = new Card("3", "Hearts", 3);
-  
-  		System.out.println("One matches two?" + one.matches(two));
-  		System.out.println("One matches three?" + one.matches(three));    
-  		System.out.println("Suit of Card One: " + one.suit());
-  		System.out.println("Card one: " + one.toString());
- 
+		Card1 c1 = new Card1("Queen", "Hearts", 10);
+		Card1 c2 = new Card1("King", "Spades", 10);
+		Card1 c3 = new Card1("Queen", "Hearts", 10);
+
+		System.out.println(c1.rank());
+		System.out.println(c1.suit());
+		System.out.println(c1.pointValue());
+
+		System.out.println(c1.matches(c2));
+		System.out.println(c1.matches(c3));
+
+		System.out.println(c2);
 	}
 }
